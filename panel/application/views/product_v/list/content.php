a63c9153b206de03c9b262d0fff449f8415bd516
Huseyin Selen <selen.huseyin98@gmail.com>
2024-10-21 17:12:18 +0300

    urunler modulunun urun listesi icin html sablonu olusturuldu.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,55 @@
-urun listesi content
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Ürün Listesi</h4>
+    </div><!-- END column -->
+
+    <div class="col-md-12">
+        <div class="widget p-lg">
+
+            <table class="table table-hover table-striped">
+                <thead>
+                    <th>#id</th>
+                    <th>url</th>
+                    <th>Başlık</th>
+                    <th>Açıklama</th>
+                    <th>Durumu</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Maonitör Askısı</td>
+                        <td>360 Derece kullanılan monitör askısı</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#">Sil</a>
+                            <a href="#">Düzenle</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Maonitör Askısı</td>
+                        <td>360 Derece kullanılan monitör askısı</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#">Sil</a>
+                            <a href="#">Düzenle</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Maonitör Askısı</td>
+                        <td>360 Derece kullanılan monitör askısı</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#" class="btn btn-xs btn-danger btn-outline"><i class="fa fa-trash"></i>Sil</a>
+                            <a href="#" class="btn btn-xs btn-primary btn-outline"><i class="fa fa-pencil-square-o"></i>Düzenle</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+</div>
