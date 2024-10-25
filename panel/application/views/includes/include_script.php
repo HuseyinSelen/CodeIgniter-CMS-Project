9e94e6aeacd16f351de8e5c17d56a064b9add38b
Huseyin Selen <selen.huseyin98@gmail.com>
2024-10-25 17:24:56 +0300

    library.js dosyası library.php isimli php klasörüne taşındı (base_url kullanabilmek için). Urun eklemek icin gerekli olan view tasarımı gerceklestirildi.

--- panel/application/views/includes/include_script.php
+++ panel/application/views/includes/include_script.php
@@ -8,8 +8,9 @@
 <script src="<?php echo base_url("assets"); ?>/libs/bower/PACE/pace.min.js"></script>
 <!-- endbuild -->
 
-<!-- build:js /assets/js/app.min.js -->
-<script src="<?php echo base_url("assets"); ?>/assets/js/library.js"></script>
+<!-- build:js <?php echo base_url("assets"); ?>/assets/js/app.min.js -->
+<!-- <script src="<?php echo base_url("assets"); ?>/assets/js/library.js"></script> -->
+<?php $this->load->view("includes/library"); ?>
 <script src="<?php echo base_url("assets"); ?>/assets/js/plugins.js"></script>
 <script src="<?php echo base_url("assets"); ?>/assets/js/app.js"></script>
 <!-- endbuild -->
