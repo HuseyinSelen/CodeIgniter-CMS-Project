4145c6475ec2a20126a10f99b9d0d97a41e71c08
Huseyin Selen <selen.huseyin98@gmail.com>
2024-10-28 11:13:02 +0300

    urune ait gorsellerin url dosyalari SEO uyumlu hale getirildi.

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -244,8 +244,17 @@ class Product extends CI_Controller
     public function image_upload($id)
     {
 
+
+        //$file_name = convertToSEO($_FILES["file"]["name"]);
+
+        $file_name = convertToSEO(pathinfo($_FILES["file"]["name"], PATHINFO_FILENAME)) . "." . pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION);
+
+
         $config["allowed_types"] = "jpg|jpeg|png";
         $config["upload_path"] = "uploads/$this->viewFolder/";
+        $config["file_name"] = $file_name;
+
+
         $this->load->library("upload", $config);
 
         $upload = $this->upload->do_upload("file");
